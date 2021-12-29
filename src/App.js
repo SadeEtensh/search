965f4e409cf739e36865aaea4d997bd8e316695d
SadeEtensh <eteneshsade550305@gmail.com>
2021-12-29 11:01:56 +0000

    React search fuc

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+import JSONDATA from './MOCK_DATA.json'
+import { useState} from 'react';
+
+function App() {
+  const [serachTerm, setSearchTerm] = useState('');
+  return <div className='App'><input type="text" placeholder="Search..." onChange={event =>{
+    setSearchTerm(event.target.value);
+  }} />
+  {JSONDATA.filter(val=> {
+    if(serachTerm ==""){
+      return val
+    } else if (val.first_name.toLowerCase().includes(serachTerm.toLowerCase())){
+      return val
+    }
+  }).map((val, key) => {
+    return<div><p>{val.first_name}</p></div>;
+  })}
+  </div>
+}
+
+export default App;
